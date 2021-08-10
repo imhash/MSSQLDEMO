074128d4d4e54556f8a30493e7f2f172d44c8fd0
imhash <nphashim@live.com>
2021-08-10 15:16:02 +0400

    Create 06.test1.sql

---
+++ 06.test1.sql
@@ -0,0 +1,10 @@
+CREATE TABLE [dbo].[test1](
+	[id] [int] IDENTITY(1,1) PRIMARY KEY,
+	[name] [nchar](30) NULL,
+	[mid_name] [nchar](30) NULL,
+	[surname] [nchar](30) NULL,
+	[age] [smallint] NULL,
+	[address] [nchar](100) NULL,
+	[city] [nchar](30) NULL,
+	[country] [nchar](30) NULL
+);
